f87c1c34e914ead8d2c8f189f5ac41955652ebf4
Jery <mr.jerylee@gmail.com>
2018-09-02 16:46:50 +0700

    update Implement Asycntask

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -20,14 +20,10 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -134,4 +130,5 @@ public class EarthquakeActivity extends AppCompatActivity {
             }
         }
     }
+
 }
